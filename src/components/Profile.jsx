7557ef3af6322b2254b7a23c5308d5bb60827be4
Dmytro_Kolotilin <dkolotilin@eva.ua>
2025-05-19 18:21:47 +0300

    add hw-1

---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import css from "./Product.module.css";
+
+export default function Profile({ name, tag, location, image, stats }) {
+  return (
+    <div className={css.profile}>
+      <div className={css.description}>
+        <img src={image} alt={name} className={css.avatar} />
+        <p className={css.username}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+
+      <ul className={css.stats}>
+        <li>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
+        </li>
+        <li>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
+        </li>
+        <li>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
